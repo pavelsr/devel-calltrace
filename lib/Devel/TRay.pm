e0dc2e847e336061ff8313b6f46b50637f8bcd32
Pavel Serikov <pavelsr@cpan.org>
2019-01-08 18:58:39 +0300

    Removed comments and debugger flags

--- lib/Devel/TRay.pm
+++ lib/Devel/TRay.pm
@@ -9,10 +9,7 @@ Devel::TRay - See what your code's doing
 or
 
     perl -d:TRay script.pl
-
-=head1 INSTALLATION
-
-    perl Makefile.PL && sudo make install
+    
 
 =head1 DESCRIPTION
 
@@ -34,38 +31,6 @@ our $VERSION = '1.0';
 
 $SUBS_MATCHING = qr/.*/;
 
-# From perldoc perlvar
-# Debugger flags, so you can see what we turn on below.
-#
-# 0x01  Debug subroutine enter/exit.
-# 
-# 0x02  Line-by-line debugging.
-# 
-# 0x04  Switch off optimizations.
-# 
-# 0x08  Preserve more data for future interactive inspections.
-# 
-# 0x10  Keep info about source lines on which a subroutine is defined.
-# 
-# 0x20  Start with single-step on.
-# 
-# 0x40  Use subroutine address instead of name when reporting.
-# 
-# 0x80  Report "goto &subroutine" as well.
-# 
-# 0x100 Provide informative "file" names for evals based on the place they were com-
-#         piled.
-# 
-# 0x200 Provide informative names to anonymous subroutines based on the place they
-#         were compiled.
-# 
-# 0x400 Debug assertion subroutines enter/exit.
-# 
-  
-
-
-BEGIN { $^P |= (0x01 | 0x80 | 0x100 | 0x200); };
-
 sub import {
     my ( $self, $re ) = @_;
 
@@ -75,63 +40,19 @@ sub import {
 }
 
 package DB;
-
-# Any debugger needs to have a sub DB. It doesn't need to do anything.
 sub DB{};
-
-# We want to track how deep our subroutines go
 our $CALL_DEPTH = 0;
 
-
-=head2 DB::sub
-
-perl will automatically call DB::sub on each subroutine call and leave it up
-to us to dispatch to where we want to go.
-
-=cut
-
-
 sub sub {
-    # localize CALL_DEPTH so that we don't need to decrement it after the sub 
-    # is called
     local $DB::CALL_DEPTH = $DB::CALL_DEPTH+1;
-
-    # Report on what's going on, but only if it matches our regex
     Devel::TRay::called($DB::CALL_DEPTH, \@_) 
         if ($DB::sub =~ $Devel::TRay::SUBS_MATCHING);
-
-    # Call our subroutine. @_ gets passed on for us.
-    # by calling it last, we don't need to worry about "wantarray", etc
-    # by returning it like this, the caller's expectations are conveyed to 
-    # the called routine
     &{$DB::sub};
 }
 
-=head2 Devel::TRay::called 
-
-This routine is called with two parameters:
-
-=over
-
-=item DEPTH
-
-The integer "depth" that this call is being called at.
-
-=item PARAMS
-
-A reference to the routine's @INC
-
-=back
-
-To get at the subroutine that was being called, have a look at $DB::sub
-
-=cut
 
 sub Devel::TRay::called {
     my ( $depth, $routine_params ) = @_;
-    # print STDERR is safe. warn is not. calling any routine 
-    # not defined from within the DB:: package will not work. (see perldebguts)
-    
     if ( $DB::sub !~ /CODE/ ) {
         print STDERR " " x $depth . $DB::sub;
         if (exists $DB::sub{$DB::sub}) {
@@ -141,31 +62,4 @@ sub Devel::TRay::called {
     }
 }
 
-=head1 BUGS
-
-It uses the debugger. How could it not have bugs?
-
-=head1 SEE ALSO
-
-L<perldebguts>, L<DB>, a licensed therapist.
-
-
-L<Devel::XRay> - Uses source filters. Scares me.
-
-L<Devel::TraceCalls> - Very robust API. The code seems to do all sorts of scary
-magic
-
-
-L<Debug::Trace> - Uses symbol table magic to wrap your functions. 
-
-L<Devel::TRaceFuncs> - Requires developers to instrument their source files.
-
-
-=head1 COPYRIGHT
-
-Copyright 2005 Jesse Vincent <jesse@bestpractical.com>
-
-This module may be redistributed under the same terms as perl itself
-
-=cut
 1;
